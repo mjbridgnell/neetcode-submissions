ec7d7c6086d44cfda1949251da1a88f2ece0c65b
Maxwell Bridgnell <112014755+mjbridgnell@users.noreply.github.com>
2026-05-01 15:23:51 -0700

    Add: minimum-stack - submission-7

---
+++ Data Structures & Algorithms/minimum-stack/submission-7.cpp
@@ -0,0 +1,35 @@
+class MinStack {
+    stack<int> m_stack;
+    stack<int> e_stack;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(!m_stack.empty())
+        {
+            int cur = e_stack.top();
+            e_stack.push(min(cur, val));
+            cout << e_stack.top() << endl;
+        }
+        else
+        {
+            e_stack.push(val);
+        }
+        m_stack.push(val);
+    }
+    
+    void pop() {
+        m_stack.pop();
+        e_stack.pop();
+    }
+    
+    int top() {
+        return m_stack.top();
+    }
+    
+    int getMin() {
+        return e_stack.top();
+    }
+};
