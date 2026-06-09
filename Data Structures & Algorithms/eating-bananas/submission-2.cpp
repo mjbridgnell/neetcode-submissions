4d7338d19d685ab12b5a64072ac42c9c686eff2d
Maxwell Bridgnell <112014755+mjbridgnell@users.noreply.github.com>
2026-06-09 15:31:14 -0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int high = *max_element(piles.begin(), piles.end());
+        int low = 1;
+        int mid = 1;
+        
+        while (high >= low)
+        {
+            mid = ((high - low) / 2) + low;
+            int hours = 0;
+            for (int n : piles)
+            {
+                hours += ceil(static_cast<double>(n) / mid);
+            }
+            
+            if (hours > h)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+};
